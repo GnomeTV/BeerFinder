8cefdbbcfb84b03973bf691c20aab8fb3404236b
herasima <melnikov.ivan2810@gmail.com>
2020-07-16 16:34:43 +0300

    added a correct search for bars in the specified area; switch to Yandex API; added new inline and standard buttons; removed Procfile; expanded .gitgnore

--- main.py
+++ main.py
@@ -1,80 +1,106 @@
 import telebot
 import requests
 import json
+import API
 import re
 from telebot import types
-import os
 
-token = os.environ.get("TOKEN",)
-api_google = os.environ.get("API_GOOGLE",)
-bot = telebot.TeleBot(token)
-glob_req = None
-cord = None
+bot = telebot.TeleBot(API.token)
 
 
 @bot.message_handler(commands=['start'])
 def help_com(message):
     start_key = types.ReplyKeyboardMarkup()
-    start_key.row('Rating Beer')
-    start_key.row('Find Bar')
+    start_key.row('Найти бар поблизости')
+    start_key.row('Оставить отзыв', 'Поставить оценку бару')
     bot.send_message(message.from_user.id, "Выберите пункт", reply_markup=start_key)
 
 
 @bot.message_handler(content_types=['text'])
-def key_answ(message):
-    if message.text == 'Find Bar':
-        bot.send_message(message.chat.id, "Желаемый радиус поиска (в метрах)")
+def key_answer(message):
+    if message.text == 'Найти бар поблизости':
+        bot.send_message(message.chat.id, 'Не забудь ввести радиус поиска (в метрах)')
+
+    elif re.match(r'\d{' + str(len(message.text)) + '}', message.text) != None:
+        latitude = int(message.text) / (1000 * 111)
+        longitude = int(message.text) / (1000 * 62.6)
+        global part_of_requeuest
+        part_of_requeuest = str(longitude) + ',' + str(latitude)
+        keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+        button_geo = types.KeyboardButton(text="И где же ты?", request_location=True)
+        button_home = types.KeyboardButton(text="Главное меню")
+        keyboard.add(button_geo)
+        keyboard.add(button_home)
+        bot.send_message(message.chat.id, 'Жду твое гео🗿', reply_markup=keyboard)
+    elif message.text == 'Оставить отзыв':
+        keyboard = types.InlineKeyboardMarkup()
+        url_button = types.InlineKeyboardButton(text="Оставить отзыв в чате", url="https://t.me/beer_feedback")
+        keyboard.add(url_button)
+        bot.send_message(message.chat.id, "Чтобы оставить отзыв, просто ткни на кнопку!", reply_markup=keyboard)
+    elif message.text == 'Поставить оценку бару' or message.text == 'Сорта пива':
+        bot.send_message(message.chat.id, "Находится в разработке...")
+        bot.send_message(message.chat.id, "Поддержи проект, чтобы мы перестали пить пиво и занялись проектом")
+    elif message.text == 'Главное меню':
+        start_key = types.ReplyKeyboardMarkup()
+        start_key.row('Найти бар поблизости', 'Поставить оценку бару')
+        start_key.row('Оставить отзыв')
+        bot.send_message(message.from_user.id, "Выберите пункт", reply_markup=start_key)
     else:
-        global rad
-        rad = message.text
-        if re.match('\d'+'{'+str(len(message.text))+'}', rad) != None:
-            keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-            button_geo = types.KeyboardButton(text="Отправить местоположение", request_location=True)
-            keyboard.add(button_geo)
-            bot.send_message(message.chat.id, 'Отправь геолокацию, посмотреть ближайшие бары в радиусе ' + rad +'м',
-                             reply_markup=keyboard)
-        else:
-            bot.send_message(message.chat.id, "Ошибка, попробуйте снова")
-        print(rad)
+        bot.send_message(message.chat.id, "Чел, не пори дичь. Го выпьем пивка 🍻")
 
 
 @bot.message_handler(content_types=['location'])
 def find_bar(message):
     if message.location is not None:
         start_key = types.ReplyKeyboardMarkup()
-        start_key.row('Rating Beer')
-        start_key.row('Find Bar')
-        req = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' + str(
-            message.location.latitude) + ',' + str(
-            message.location.longitude) + '&radius='+rad+'&type=bar&key=' + api_google
+        start_key.row('Найти бар поблизости')
+        start_key.row('Оставить отзыв', 'Поставить оценку бару')
+        req = 'https://search-maps.yandex.ru/v1/?apikey=' + API.yand + '&text=бар&lang=ru_RU&ll=' + str(
+            message.location.longitude) + ',' + str(message.location.latitude) + '&spn=' + part_of_requeuest + '&rspn=1'
+        user_name = str(message.from_user.username)
         response = requests.get(req)
         js_bars = response.text
         js_bars = json.loads(js_bars)
-        # js_user = str(message.location)
-        # print(js_user)
-        # print(type(js_user))
-        # js_us = json.loads(js_user)
-        # print(js_us)
         cord = str(message.location.latitude) + ',' + str(message.location.longitude)
+        print(user_name)
         print(req)
-        if js_bars['results'] != None:
-            for i in range(0, len(js_bars['results'])):
-                lat = js_bars['results'][i]['geometry']['location']['lat']
-                lng = js_bars['results'][i]['geometry']['location']['lng']
-                bar_name = js_bars['results'][i]['name']
-                bot.send_message(message.from_user.id, str(bar_name))
+        if js_bars['features'] != 0:
+            for i in range(0, len(js_bars['features'])):
+                bar_stat = ''
+                bar_hours = ''
+                bar_link = ''
+                bar_category = ''
+                bar_name = js_bars['features'][i]['properties']['name']
+                bar_address = js_bars['features'][i]['properties']['CompanyMetaData']['address'] + ';'
+                bar_stat = bar_name + '\n' + bar_address + '\n'
                 try:
-                    rating = js_bars['results'][i]['rating']
-                    bot.send_message(message.from_user.id, 'Рейтинг: ' + str(rating))
+                    bar_hours = js_bars['features'][i]['properties']['CompanyMetaData']['Hours']['text']
+                    bar_stat += bar_hours + '\n'
                 except:
-                    bot.send_message(message.from_user.id, 'Рейтинг: ' + '-')
-                bot.send_location(message.from_user.id, latitude=float(lat), longitude=float(lng))
-        elif js_bars['status'] == 'OVER_QUERY_LIMIT':
-            bot.send_message(message.from_user.id, "Превышен лимит запросов, попробуйте снова через 20 секунд",
-                             reply_markup=start_key)
-        elif js_bars['status'] == 'ZERO_RESULTS':
-            bot.send_message(message.from_user.id, "Нет баров поблизости",
-                             reply_markup=start_key)
+                    bar_hours = 'Часы работы не указаны'
+                try:
+                    bar_link = js_bars['features'][i]['properties']['CompanyMetaData']['url']
+                    bar_stat += bar_link + '\n'
+                except:
+                    bar_link = 'Ссыка на сайт бара не указана'
+                print('Перед котигориями i = ' + str(i))
+                try:
+                    for j in range(0, len(js_bars['features'][i]['properties']['CompanyMetaData']['Categories'])):
+                        bar_category += js_bars['features'][i]['properties']['CompanyMetaData']['Categories'][j][
+                                            'name'] + ';'
+                    bar_stat += bar_category
+                except:
+                    bar_category = 'Категория у бара не указана'
+                    bar_stat += bar_category
+
+                bot.send_message(message.from_user.id, str(bar_stat))
+                lat = js_bars['features'][i]['geometry']['coordinates'][1]
+                lng = js_bars['features'][i]['geometry']['coordinates'][0]
+                print(float(lat), ' , ', float(lng))
+                bot.send_venue(message.chat.id, latitude=float(lat), longitude=float(lng), title=bar_name,
+                               address=bar_address)
+        else:
+            bot.send_message(message.from_user.id, "Нет баров поблизости", reply_markup=start_key)
         bot.send_message(message.from_user.id, "Выберите пункт", reply_markup=start_key)
 
 
